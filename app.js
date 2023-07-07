11a4488e02374fa7cb67a47a56fabc90996839c5
MattyBoySales <matthewrileysales1@outlook.com>
2023-07-06 19:12:31 -0600

    Fixed the 'Our Services' Section

--- app.js
+++ app.js
@@ -102,3 +102,25 @@ scrollLinks.forEach(function(link) {
     // Open the mailto link in a new window or tab
     window.open(mailtoLink)
   }
+
+  /*SERVICES TABS SECTION */
+
+  const about = document.querySelector(".about");
+const btns = document.querySelectorAll(".tab-btn");
+const articles = document.querySelectorAll(".content");
+about.addEventListener("click", function (e) {
+  const id = e.target.dataset.id;
+  if (id) {
+    // remove selected from other buttons
+    btns.forEach(function (btn) {
+      btn.classList.remove("active");
+    });
+    e.target.classList.add("active");
+    // hide other articles
+    articles.forEach(function (article) {
+      article.classList.remove("active");
+    });
+    const element = document.getElementById(id);
+    element.classList.add("active");
+  }
+});
